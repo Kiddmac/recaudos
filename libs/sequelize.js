973dcd7935d9eea9886df220385d99251b45ba9c
Kiddmac <ronaldphw@gmail.com>
2022-05-26 21:57:37 -0500

    [DEPLOY] se realizó la configuración necesaria para el deploy a HEROKU

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,15 +1,28 @@
+const { unauthorized } = require('@hapi/boom');
 const { Sequelize } = require('sequelize');
 const {config} = require('../config/config')
 const setupModels = require('../db/models/index')
 
+let URI = '';
+ 
+if(config.isProd){
+    URI = config.dbUrl
+}
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-const sequelize = new Sequelize(URI, {
+
+const options = {
     dialect: 'postgres',
-    logging: true
-})
+    logging: config.isProd ? false : true,
+}
+if (config.isProd){
+    options.ssl = {
+        rejectUnauthorized: false
+    }
+}
+const sequelize = new Sequelize(config.dbUrl, options)
 
 setupModels(sequelize);
 sequelize.sync();
